2e807c16f03864f4af1d851e3d83749a0f70c912
Rumen Guin <rumen2k@gmail.com>
2023-05-22 19:50:21 +0530

    Initial Commit

---
+++ Grocery/GroceryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryApp.swift
+//  Grocery
+//
+//  Created by RUMEN GUIN on 22/05/23.
+//
+
+import SwiftUI
+
+@main
+struct GroceryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
